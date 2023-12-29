ff181bb57adaffa5b2a44b8c364ecb8970154983
saewoonkim <30545132+saewoonkim@users.noreply.github.com>
2023-12-29 17:49:04 +0900

    Add files via upload

---
+++ 2023 12 29/Ex02_02.py
@@ -0,0 +1,63 @@
+"""
+kor = 80
+eng = 75
+math = 55
+print("홍길동씨의 평균 점수는 ", ((kor+eng+math)/3), "입니다")
+
+a = 13
+if a % 2 == 0:
+    print('짝수')
+else:
+    print('홀수')
+    
+pin = "881120-1068234"
+yyyymmdd = "19" + pin[0:6]
+num = pin[7:]
+print(yyyymmdd)
+print(num)
+
+pin = "881120-1068234"
+print(pin[7])
+
+a = "a:b:c:d"
+b = a.replace(":", "#")
+print(b)
+
+
+a = [1,3,5,4,2]
+a.sort()
+a.reverse()
+print(a)
+
+
+a = ['life','is','too','short']
+result = " ".join(a)
+print(result)
+
+a = (1,2,3)
+a = a + (4,)
+print(a)
+
+a = {'A':90, 'B':80, 'C':70}
+result = a.pop('B')
+print(a)
+print(result)
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 2023 12 29/Ex03_01.py
@@ -0,0 +1,79 @@
+"""
+if 1 < 2:
+    print('참')
+    print('그러쿠나')
+    
+print('그래 그래')
+"""
+"""money = 2000
+card = True
+if money >= 3000 or card:
+    print("택시 고고")
+else :
+    print("버스 고고")
+
+a = int(input("숫자입력 : "))
+
+if a % 3 == 0 or a % 5 == 0:
+    print(a)
+else:
+    print()
+
+num1 = int(input('숫자 1 입력 : '))
+num2 = int(input('숫자 2 입력 : '))
+
+if num1 > num2:
+    print(num1)
+else :
+    print(num2)
+
+
+num1 = int(input('숫자를 입력하세요 : '))
+
+if num1 % 2 == 0:
+    print("%d는 짝수입니다." % num1)
+else :
+    print("%d는 홀수입니다." % num1)
+
+    
+num1 = input("숫자 1 입력 : ")
+num2 = input("숫자 2 입력 : ")
+
+print("%d + %d = %d " % (num1, num2, num1+num2))
+print("%d - %d = %d " % (num1, num2, num1-num2))
+print("%d x %d = %d " % (num1, num2, num1*num2))
+print("%d / %d = %.2f " % (num1, num2, num1/num2))
+print("%d %% %d = %d " % (num1, num2, num1%num2))
+print("%d // %d = %d " % (num1, num2, num1//num2))
+
+num1 = int(input("숫자 1 입력 : "))
+num2 = int(input("숫자 2 입력 : "))
+oper = input("연산자 입력 : ")
+
+if oper == "+":
+    print(num1+num2)
+elif oper == "-":
+    print(num1-num2)
+elif oper == "*":
+    print(num1*num2)
+elif oper == "/":
+    print(num1/num2)
+else :
+    print("연산자 다시 입력")
+"""
+
+year = int(input("연도 입력 : "))
+
+if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
+    print("입력하신  %d 년은 윤년입니다. " % year)
+else : 
+    print("입력하신  %d 년은 윤년이 아닙니다. " % year)
+
+
+
+
+
+
+
+
+
