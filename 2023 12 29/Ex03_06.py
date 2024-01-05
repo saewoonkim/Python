c878f2c798a0b398b5bed12e1b73dc00b15fd7e0
saewoonkim <30545132+saewoonkim@users.noreply.github.com>
2024-01-05 17:48:08 +0900

    Add files via upload

---
+++ 2023 12 29/Ex03_06.py
@@ -0,0 +1,78 @@
+"""
+class Calculator:
+    def __init__(self):
+        self.result = 0
+
+    def add(self, num):
+        self.result += num
+        return self.result
+
+cal1 = Calculator()
+cal2 = Calculator()
+
+print(cal1.add(3))
+print(cal1.add(4))
+print(cal2.add(3))
+print(cal2.add(7))
+
+class FourCal:
+    def __init__(self, first, second):
+        self.first = first
+        self.second = second
+    def add(self):
+        result = self.first + self.second
+        return result
+    def mul(self):
+        result = self.first * self.second
+        return result
+    def sub(self):
+        result = self.first - self.second
+        return result
+    def div(self):
+        result = self.first / self.second
+        return result
+
+
+ 
+def add(a,b):
+    return a + b
+def sub(a,b):
+    return a - b
+
+
+    
+
+if __name__  == "__main__":
+    print(add(1,4))
+    print(sub(4,2))
+
+
+    
+"""
+
+PI = 3.141592
+
+class Math:
+    def solv(self, r):
+        return PI * (r ** 2)
+
+def add(a,b):
+    return a + b
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
